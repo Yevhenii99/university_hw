0b3e32b463649e1bc717dca0870003800f316e82
unknown <evgengorobets5@gmail.com>
2023-04-02 21:22:39 +0300

    cctor added

--- hw_10.cpp
+++ hw_10.cpp
@@ -2,15 +2,19 @@
 #pragma warning(disable : 4996)
 #include "myHeader.h"
 
+
 int main(void) {
 	Magazine news(76.3, "VoiceOfAmerika", 132);
 	//Magazine news2;//Will be called default ctor in base class and after in derivative class 
 	Magazine news3(13, 67, "RealNews");//Will be called ctor from inheritance
+	Magazine obj("SOmething", 255);
 	//news.Show();
 	//news3.Show();
-	news.SetAmountOfEd(1050);
-	std::cout << "Amount of eddition: " << news.GetAmountOfEd() <<  std::endl;
+	//news.SetAmountOfEd(1050);
+	//Magazine news4(44, 55, 666);
+	//Magazine news5(news4);
+	//news4.Show();
+	//news5.Show();
+	//std::cout << "Amount of eddition: " << news.GetAmountOfEd() <<  std::endl;
 	//news.ShowBook();//If i define access to class Book like privat or protected i won`t be able to use methods from Book anywhere
-	Magazine news4 = news;
-	news4.Show();
 }
--- myHeader.h
+++ myHeader.h
@@ -14,10 +14,11 @@ public:
 		this->nameOfBook = new char[strlen(name) + 1];
 		strcpy(nameOfBook, name);
 	}
-	Book(const char* nameOfBook, unsigned short pages) :pages(pages) {
-		this->nameOfBook = new char[strlen(nameOfBook) + 1];
-
-		strcpy(this->nameOfBook, nameOfBook);
+	Book(const char* bookName, unsigned short pages) :pages(pages) {
+		/*this->nameOfBook = new char[strlen(nameOfBook) + 1];*/
+		/*strcpy(this->nameOfBook, nameOfBook);*/
+		nameOfBook = strdup(bookName);
+		std::cout << "Book name value: " << bookName << std::endl;
 	}
 	void ShowBook() {
 		std::cout << "Name: " << nameOfBook << " Pages: " << pages << std::endl;
@@ -26,26 +27,34 @@ public:
 		amountOfEddition = amount;
 	}
 	unsigned short GetAmountOfEd() { return amountOfEddition; }
-	Book(const Book& value):pages(value.pages), amountOfEddition(value.amountOfEddition), lengthOfItem(value.lengthOfItem), widthOfItem(value.widthOfItem) {
-		nameOfBook = new char[strlen(nameOfBook) + 1];
-		this->nameOfBook = nameOfBook;
+	Book(char widthOfItem, char lengthOfItem, unsigned short pages):widthOfItem(widthOfItem), lengthOfItem(lengthOfItem), pages(pages) {
+		nameOfBook = strdup("default name");
+		
+	}
+	//cctor
+	Book(const Book& object) :pages(object.pages), lengthOfItem(lengthOfItem), widthOfItem(widthOfItem) {
+		//std::cout << "Cctor class Book!\n";
+		//nameOfBook = new char[strlen(object.nameOfBook) + 1];
+		nameOfBook = strdup(object.nameOfBook);
+		//strcpy(nameOfBook, object.nameOfBook);
+
 	}
 	~Book() {
 		delete[] nameOfBook;
-		std::cout << "Book destr work!\n";//Will be called second after Magazine destructor
+		//std::cout << "Book destr work!\n";//Will be called second after Magazine destructor
 	}
 };
 class Magazine :public Book
 {
-	float price;
+	float price = 0;
 public:
 	Magazine(float price, const char* nameOfBook, unsigned short pages) :Book(nameOfBook, pages), price(price) { std::cout << "Ctor param1 Magazine!\n"; }
 	void Show() {
 		std::cout << "Price: " << price << std::endl;
-		Book::ShowBook();
+		ShowBook();
 	}
 	using Book::Book;//Inheritance of ctor
-	Magazine() :price(15.6) { std::cout << "Ctor default Magazine!\n"; }
-	Magazine(const Magazine& object):price(66.6), Book(object) {}
-	~Magazine() { std::cout << "Magazine destr work\n"; }//Will be called first before Book destr
+	//cctor
+	Magazine(Book& object) :Book(object) { std::cout << "Cctor magazine!"; }
+	~Magazine() { /*std::cout << "Magazine destr work\n"; */ }//Will be called first before Book destr
 };
