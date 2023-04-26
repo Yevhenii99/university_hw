52310d3f7ea132e245d28b13f391af2566fd2f92
unknown <evgengorobets5@gmail.com>
2023-04-26 17:02:11 +0300

    def templates for class, func

--- header_13.h
+++ header_13.h
@@ -13,15 +13,48 @@ public:
 		nameOfEdition = _strdup(name);
 	}
 	void Show() {
-		std::cout << yearOfCreated << "\n" << employeesAmount << "\n" << budget << "\n" << bookCirculation << "\n" << nameOfEdition << std::endl;
+		std::cout << yearOfCreated << " " << employeesAmount << " " << budget << " " << bookCirculation << " " << nameOfEdition << std::endl;
 	}
 	~PrintedEdition() { delete[] nameOfEdition; }
 };
 
-template <typename Value1, typename Value2>
+//Task2
+template <typename Value1, typename Value2 = float>
 
 Value2 getMaxValue(Value1 x, Value2 y, Value1 z) {
 	if (x > y && x > z) return x;
 	else if (y > x && y > z) return y;
 	else if (z > x && z > y) return z;
 }
+//Task3
+template <typename Value1, typename Value2>
+
+Value1 getSumOfNums(Value2 x, Value2 y) {
+	return x + y;
+}
+//Task4
+template <typename T1> 
+
+T1 getSumOfElements(T1* arr, int row, int col) {
+	T1 sum = 0;
+	for (short i = 0; i < row; ++i)
+	{
+		for (short j = 0; j < col; ++j) {
+			sum += *(arr + i*col + j);
+		}
+	}
+	return sum;
+}
+
+//Task5
+
+template <typename T1 = int, typename T2 = double>
+class Circle {
+	T1 r = 0;
+	const T2 P = 3.14;
+public:
+	Circle(T1 radius) :r(radius) {};
+	T2 getLengthOfCircle() {
+		return 2 * P * r;
+	}
+};
--- hw_13.cpp
+++ hw_13.cpp
@@ -10,4 +10,29 @@ int main(void) {
 	std::cout << getMaxValue(10, 14, 8) << std::endl;;
 	//Different type of parameters
 	std::cout << getMaxValue(23, 4.7f, 16) << std::endl;
+	std::cout << getSumOfNums<int>(20, 10) << '\n';
+	std::cout << getSumOfNums<float>(20.6f, 10.1f) << '\n';
+	//2D array
+
+	int arr[3][2] = {
+		{1,2},
+		{3,4},
+		{5,16}
+	};
+	float arr3[2][4] = {
+	{1, 2.5f, 3.7f, 4},
+	{5, 6.5f, 7.5f, 3.9f},
+	};
+	double arr4[3][4] = {
+	{1.5, 2.5, 3.7, 4},
+	{5, 6.5, 7.5, 3.9},
+	{5.2, 6.5, 7.1, 2.9},
+	};
+	std::cout << "Int sum: " << getSumOfElements((int*)arr, 3, 2) << '\n';
+	std::cout << "Float sum: " << getSumOfElements((float*)arr3, 2, 4) << '\n';
+	std::cout << "Double sum: " << getSumOfElements((double*)arr4, 3, 4) << '\n';
+	//Task5
+	Circle<> circle1(5);
+	std::cout << circle1.getLengthOfCircle() << " cm\n";
+
 }
